a25c041d5c257736a407c581c51a117b9e5f1b48
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-02-09 23:04:33 -0500

    combined js

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-  var c1score = 0;
-  var c2score = 0;
-  var c3score = 0;
-  var c4score = 0;
+var c1score = 0;
+var c2score = 0;
+var c3score = 0;
+var c4score = 0;
+var type =[];
+var typeindex = 0;
+
+//var counter = 0;
+
+function determineType(){
+    var choices = document.getElementsByTagName('input');
+    for (i=0; i<choices.length; i++){
+        if (choices[i].checked){
+            if (choices[i].value == 'longevity'){
+                type.push('longevity');
+//                counter = counter+1;
+//                if(counter>=2){break;}
+            }
+            if (choices[i].value == 'stability'){
+                type.push('stability');
+//                counter = counter+1;
+//                if(counter>=2){break;}
+            }
+            if (choices[i].value == 'satisfaction'){
+                type.push('satisfaction');
+//                counter = counter+1;
+//                if(counter>=2){break;}
+            }
+            if (choices[i].value == 'salary'){
+                type.push('salary');
+//                counter = counter+1;
+//                if(counter>=2){break;}
+            }
+        }
+    }
+    if (type.includes("salary")&& type.includes("satisfaction")){typeindex = 1}
+    if (type.includes("salary")&& type.includes("stability")){typeindex = 2}
+    if (type.includes("longevity")&& type.includes("stability")){typeindex = 3}
+    if (type.includes("longevity")&& type.includes("satisfaction")){typeindex = 4}
+    if (type.includes("salary")&& type.includes("longevity")){typeindex = 5}
+    if (type.includes("stability")&& type.includes("satisfaction")){typeindex = 6}
+
+    }
+
+
+if (typeindex == 1) { 
+      answerbox.innerHTML = "salary+satisfaction";
+      answerbox.style.backgroundColor="green";
+      answerbox.style.fontSize='24px';
+      cityscape.style.opacity='1';
+      cityscape.style.position
+     }
 
 function tabulateAnswers() {
   // initialize variables for each choice's score
@@ -59,12 +107,13 @@ function scrolltopos(){
     
 // TRING SIDE NAV  
 function openNav() {
-  document.getElementById("plswork").style.height = "70vh";
-  document.getElementsByClassName("questions").style.height = "70vh";
+  document.getElementById("plswork").setAttribute("style","height:70vh");
+//  document.getElementsByClassName("questions").setAttribute("style","height:70vh");
 }
 
 function closeNav() {
-  document.getElementById("plswork").style.height = "5vh";
+    document.getElementById("plswork").style.removeProperty('style');
+    document.getElementById("plswork").setAttribute("style","height:5vh");
 }
 //
 //var coll = document.getElementsByClassName("collapsible");

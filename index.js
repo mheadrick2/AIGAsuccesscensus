1e986d8e59ac0670e80f62fb7c157508fa05df9f
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-02-10 20:33:47 -0500

    change

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var c1score = 0;
-var c2score = 0;
-var c3score = 0;
-var c4score = 0;
 var type =[];
 var typeindex = 0;
 
@@ -81,33 +77,41 @@ document.getElementById("myChoice").innerHTML = declaration;}
 
 
 /////////////////   for main //////////////////////
-function tabulateAnswers() {
-  // initialize variables for each choice's score
+var choices = [];
+var imgs = [];
+var locations = [];
 
-  
-  // get a list of the radio inputs on the page
-  var choices = document.getElementsByTagName('input');
+function tabulateAnswers() {
+  var inputs = document.getElementsByTagName('input');
   // loop through all the radio inputs
-  for (i=0; i<choices.length; i++) {
+  for (i=0; i<inputs.length; i++) {
     // if the radio is checked..
-    if (choices[i].checked) {
-      // add 1 to that choice's score
-      if (choices[i].value == 'c1') {
-        c1score = c1score + 100;
+    if (inputs[i].checked) {
+      if (inputs[i].value == 'c1') {
+        choices.push("technical");
+        imgs.push();
+        locations.push(());
       }
-      if (choices[i].value == 'c3') {
-        c1score = c1score + 100;
+      if (inputs[i].value == 'c2'){
+        choices.push("communication");
+        imgs.push();
+        locations.push(());
       }
-      if (choices[i].value == 'c4') {
-        c1score = c1score + 100;
+      if (inputs[i].value == 'c3') {
+        choices.push("leadership");
+        imgs.push();
+        locations.push(());
+      }
+      if (inputs[i].value == 'c4') {
+        choices.push("thinking");
+        imgs.push();
+        locations.push(());
       }
       // If you add more choices and outcomes, you must add another if statement below.
     }
   }
   
 var canvas;
-
-
 function setup(){
     canvas = createCanvas(windowWidth,windowHeight);
     canvas.position(0,0);
@@ -115,26 +119,11 @@ function setup(){
 }
 
 function draw(){
-    background(244,252,245);
+    for (i=0; i<choices.length; i++){
+        var img = loadImage('img/'+imgs[i]+'.png');
+        image(img,locations[i][0],locations[i][1]);
+    }
 }
-  // Display answer corresponding to that choice
-  var answerbox = document.getElementById('answer5');
-  var cityscape = document.getElementById('cityscape')
-  if (c1score == '300') { 
-      answerbox.innerHTML = "The Successful Designer";
-      answerbox.style.backgroundColor="green";
-      answerbox.style.fontSize='24px';
-      cityscape.style.opacity='1';
-      cityscape.style.position
-     }
-     
-     else{
-          answerbox.innerHTML ="Not Quite Right";
-      }
- 
-    
-}
-
 
  function numberRandomizer(){
       var x = Math.floor((Math.random() * 500) + 300); //random number between 300 and 800

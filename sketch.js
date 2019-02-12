5e780b20d0f7908444b2e270c87eb5aa6ea81553
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-02-12 14:04:39 -0500

    minor changes

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 var canvas;
+var bg;
+
 function setup(){
+    bg = loadImage("img/type3.png")
     canvas = createCanvas(windowWidth*2,windowHeight*2);
     canvas.position(0,0);
     canvas.style("z-index","-1");
 }
 
 function draw(){
-    background(244,252,245);
+    background(bg);
 //    console.log("drawing..");
     for (i=0; i<allChoices.length;i++){
-        image(allImgs[i],allLocations[i][0],allLocations[i][1],allImgs[i].width/5,allImgs[i].height/5);
+        image(allImgs[i],allLocations[i][0],allLocations[i][1],allImgs[i].width/2.5,allImgs[i].height/2.5);
     }
 }
 

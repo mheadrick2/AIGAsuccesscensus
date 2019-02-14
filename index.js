3ec9d63b5e97a7d9bd06eb569ff41d02480b17c5
mheadrick <30906007+mheadrick@users.noreply.github.com>
2019-02-13 20:16:39 -0500

    modal??

--- index.js
+++ index.js
@@ -142,6 +142,47 @@ function waitscroll3() {
     moveq4();
     
 }
+//MODAL JAVASCRIPT------------------------******************
+
+
+
+    var modal = document.getElementById('floatModal');
+    var modal2 = document.getElementById('floatModal2');
+    var modcont = document.getElementsByClassName("modal-content");
+    var btn = document.getElementById("myBtn");
+    var btn2 = document.getElementById("myBtn2");
+    var span = document.getElementsByClassName("close")[0];
+    var span2 = document.getElementsByClassName("close2")[0];
+    // When the user clicks the button, open the modal 
+    btn.onclick = function(){
+      modal.style.display = "block";
+
+    }
+    
+     btn2.onclick = function(){
+        modal2.style.display="block";
+
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function()  {
+      modal.style.display = "none";
+    }
+    
+    span2.onclick = function()  {
+      modal2.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+
+
+
+
+
 //********************************SALARY AND SATISFACTION*****************************
 
 function type1q1() {

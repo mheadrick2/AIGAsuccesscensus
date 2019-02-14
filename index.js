1c03abed0baed7dcec99741e84dadfb9a57f2299
pomelofrost <38967354+pomelofrost@users.noreply.github.com>
2019-02-13 21:02:02 -0500

    l

--- index.js
+++ index.js
@@ -142,46 +142,7 @@ function waitscroll3() {
     moveq4();
     
 }
-//MODAL JAVASCRIPT------------------------******************
 
-
-//
-//    var modal = document.getElementById('floatModal');
-//    var modal2 = document.getElementById('floatModal2');
-//    var modcont = document.getElementsByClassName("modal-content");
-//    var btn = document.getElementById("myBtn");
-//    var btn2 = document.getElementById("myBtn2");
-//    var span = document.getElementsByClassName("close")[0];
-//    var span2 = document.getElementsByClassName("close2")[0];
-//    // When the user clicks the button, open the modal 
-//    btn.onclick = function(){
-//      modal.style.display = "block";
-//
-//    }
-//    
-//     btn2.onclick = function(){
-//        modal2.style.display="block";
-//
-//    }
-//    // When the user clicks on <span> (x), close the modal
-//    span.onclick = function()  {
-//      modal.style.display = "none";
-//    }
-//    
-//    span2.onclick = function()  {
-//      modal2.style.display = "none";
-//    }
-//    // When the user clicks anywhere outside of the modal, close it
-//    window.onclick = function(event) {
-//      if (event.target == modal) {
-//        modal.style.display = "none";
-//      }
-//    }
-//
-//
-//
-//
-//
 
 //********************************SALARY AND SATISFACTION*****************************
 
@@ -267,6 +228,19 @@ function type1q2() {
           var x = numberRandomizer(.7,.74);
           var y = numberRandomizer(.11,.22);
           locations2.push([windowWidth+windowWidth*x,windowHeight*y]);
+          choices2.push("freelance");
+        imgs2.push(loadImage('img/s5.png'));
+        //locations
+          var x = numberRandomizer(.7,.74);
+          var y = numberRandomizer(.11,.22);
+          locations2.push([windowWidth+windowWidth*x,windowHeight*y]);
+          choices2.push("freelance");
+        imgs2.push(loadImage('img/s5.png'));
+        //locations
+          var x = numberRandomizer(.7,.74);
+          var y = numberRandomizer(.11,.22);
+          locations2.push([windowWidth+windowWidth*x,windowHeight*y]);
+          
       }
       if (inputs[i].value == '23') {
         choices2.push("designforfriends");

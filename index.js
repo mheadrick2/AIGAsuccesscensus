8699c3c8b5ef4035b16657fd9f0a5ec21c002724
mheadrick <30906007+mheadrick@users.noreply.github.com>
2019-02-14 12:55:55 -0500

    copy

--- index.js
+++ index.js
@@ -112,11 +112,11 @@ var locations4 = [];
 // TRING SIDE NAV  
 function openNav() {
   document.getElementById("plswork").setAttribute("style","height:55vh");
-    document.getElementById("plswork2").setAttribute("style","height:60vh");
+    document.getElementById("plswork2").setAttribute("style","height:70vh");
     
     document.getElementById("plswork3").setAttribute("style","height:100vh");
     
-    document.getElementById("plswork4").setAttribute("style","height:65vh");
+    document.getElementById("plswork4").setAttribute("style","height:75vh");
 //  document.getElementsByClassName("questions").setAttribute("style","height:70vh");
 }
 
